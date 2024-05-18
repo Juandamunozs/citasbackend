2d8365bc205c6b82773b977fc1f0624dc8d9d1b4
juandamunozs <juandavidmunozsotelo7@gmail.com>
2024-05-17 22:27:28 -0500

    Correo notificacion de cita agendada

--- app/controller/CitaController.js
+++ app/controller/CitaController.js
@@ -14,39 +14,81 @@ function generarCodigo() {
 
 class CitaController {
 
-    
     async guardar(req, res) {
         const body = req.body;
         const codigo = generarCodigo();
         const citas = new Cita(body.nombre, body.apellido, body.cedula, body.numero_celular, body.email, body.direccion, body.fecha_cita, body.genero, body.hospital, body.doctor, body.notificacion, body.tipo_cita, body.hora_cita, codigo);
-        const res_guardar = await citas.guardar();
-        res.json(res_guardar);
+        
+        try {
+            const res_guardar = await citas.guardar();
+          console.log(res_guardar.respuesta);
+            // Si la cita se guarda correctamente, enviar el correo
+            if (res_guardar.exito && res_guardar.estado === 200){
+                // Ruta relativa del logo de tu empresa dentro de la carpeta 'logo'
+                const logoPath = path.join(__dirname, 'logo', 'MediCitas.png');
+                // Lee el contenido de la imagen del logo
+                const logoContent = fs.readFileSync(logoPath);
+
+                // Configurar el transporte de correo
+                let transporter = nodemailer.createTransport({
+                    service: 'Gmail', // Ejemplo: Gmail, SMTP
+                    auth: {
+                        user: 'medicitascali@gmail.com', // Coloca aquí tu dirección de correo electrónico
+                        pass: 'qrwf jlwa yjdd zuyf' // Coloca aquí tu contraseña de correo electrónico
+                    }
+                });
+
+                // Detalles del correo electrónico
+                let mailOptions = {
+                    from: 'medicitascali@gmail.com', // Dirección del remitente
+                    to: body.email, // Dirección del destinatario
+                    subject: 'Confirmación de cita en MediCita', // Asunto del correo electrónico
+                    text: `Estimado(a) ${body.nombre} ${body.apellido}, su cita ha sido confirmada para la fecha ${body.fecha_cita} a las ${body.hora_cita} con el doctor ${body.doctor} en el hospital ${body.hospital}. Su código de cita es ${codigo}. Por favor, preséntese media hora antes de su cita con su documento de identidad, el codigo es de utilidad para consultar datos o eliminar su cita en la web de MediCitas.\n\nPara cualquier duda o consulta, por favor, comuníquese a medicitascali@gmail.com`, // Cuerpo del correo electrónico
+                    attachments: [
+                        {
+                            filename: 'MediCitas.png', // Nombre del archivo adjunto
+                            content: logoContent, // Contenido de la imagen del logo
+                            encoding: 'base64' // Codificación base64
+                        }
+                    ]
+                };
+
+                // Enviar el correo electrónico
+                transporter.sendMail(mailOptions, (error, info) => {
+                    if (error) {
+                        const errorMessage = `Error al enviar el correo a ${body.nombre} ${body.apellido} por: ${error}`;
+                        res.status(500).json({ success: false, message: errorMessage });
+                    } else {
+                        const successMessage = `Correo electrónico enviado a ${body.nombre} ${body.apellido}: ${info.response}`;
+                        res.json({ success: true, message: successMessage, data: res_guardar });
+                    }
+                });
+            } else {
+                res.json(res_guardar);
+            }
+        } catch (error) {
+            res.status(500).json({ success: false, message: 'Error al guardar la cita', error });
+        }
     }
 
-
     async mostrar(req, res) {
-        const {codigo} = req.body;
-        
-        const cosultar = await Cita.consultar(codigo);
-        
-        res.json(cosultar);
+        const { codigo } = req.body;
+        const consultar = await Cita.consultar(codigo);
+        res.json(consultar);
     }
 
     async borrar(req, res) {
-        const {codigo} = req.body;
-        
+        const { codigo } = req.body;
         const borrar = await Cita.borrarCita(codigo);
-
         res.json(borrar);
     }
 
     async fecha(req, res) {
         try {
-            // Tu lógica para obtener la cita...
-            const cita = await Cita.notificacion(/*segundo, minuto, hora, dia, mes, año*/); 
-           res.json(cita);
+            const cita = await Cita.notificacion();
+            res.json(cita);
             if (cita.resultado.length > 0) {
-                cita.resultado.forEach(async (resultado, index) => {
+                cita.resultado.forEach(async (resultado) => {
                     const nombre = resultado.nombre;
                     const fecha = resultado.fechaCita;
                     const email = resultado.email;
@@ -56,56 +98,43 @@ class CitaController {
                     const hora = resultado.horaCita;
                     const codigo = resultado.codigo;
 
-                    // Ruta relativa del logo de tu empresa dentro de la carpeta 'logo'
                     const logoPath = path.join(__dirname, 'logo', 'MediCitas.png');
-                    // Lee el contenido de la imagen del logo
                     const logoContent = fs.readFileSync(logoPath);
 
-                    // Configurar el transporte de correo
                     let transporter = nodemailer.createTransport({
-                        service: 'Gmail', // Ejemplo: Gmail, SMTP
+                        service: 'Gmail',
                         auth: {
-                            user: 'medicitascali@gmail.com', // Coloca aquí tu dirección de correo electrónico
-                            pass: 'qrwf jlwa yjdd zuyf' // Coloca aquí tu contraseña de correo electrónico
+                            user: 'medicitascali@gmail.com',
+                            pass: 'qrwf jlwa yjdd zuyf'
                         }
                     });
 
-                    // Detalles del correo electrónico
                     let mailOptions = {
-                        from: 'medicitascali@gmail.com', // Dirección del remitente
-                        to: email, // Dirección del destinatario
-                        subject: 'Recordatorio de MediCita', // Asunto del correo electrónico
-                        text: 'Estimado(a) '+ nombre + ' '+ apellido +' recuerde asistir a su cita asignada con el codigo ' + codigo + ' para la fecha ' + fecha  + ' a las ' + hora +' con el doctor '+ doctor +' en el hospital ' + hospital +' debe presentarse media hora antes de su cita con su documento de identidad.\n\nPara cualquier duda o consulta, por favor, comuníquese a medicitascali@gmail.com', // Cuerpo del correo electrónico
+                        from: 'medicitascali@gmail.com',
+                        to: email,
+                        subject: 'Recordatorio de MediCita',
+                        text: `Estimado(a) ${nombre} ${apellido}, recuerde asistir a su cita asignada con el codigo ${codigo} para la fecha ${fecha} a las ${hora} con el doctor ${doctor} en el hospital ${hospital}. Debe presentarse media hora antes de su cita con su documento de identidad.\n\nPara cualquier duda o consulta, por favor, comuníquese a medicitascali@gmail.com`,
                         attachments: [
                             {
-                                filename: 'MediCitas.png', // Nombre del archivo adjunto
-                                content: logoContent, // Contenido de la imagen del logo
-                                encoding: 'base64' // Codificación base64
+                                filename: 'MediCitas.png',
+                                content: logoContent,
+                                encoding: 'base64'
                             }
                         ]
                     };
 
-                    // Enviar el correo electrónico
-                   
-     transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-               const errorMessage = 'Error al enviar el correo a ' + nombre + ' ' + apellido + ' por :' + error;
-               //console.log(errorMessage);
-               res.status(500).json({ success: false, message: errorMessage });
-        } else {
-            const successMessage = 'Correo electrónico enviado a ' + nombre + ' ' + apellido + ' :' + info.response;
-               //console.log(successMessage);
-               res.json({ success: true, message: successMessage });
-    }
-});
+                    transporter.sendMail(mailOptions, (error, info) => {
+                        if (error) {
+                            const errorMessage = `Error al enviar el correo a ${nombre} ${apellido} por: ${error}`;
+                            res.status(500).json({ success: false, message: errorMessage });
+                        } else {
+                            const successMessage = `Correo electrónico enviado a ${nombre} ${apellido}: ${info.response}`;
+                            res.json({ success: true, message: successMessage });
+                        }
+                    });
                 });
-            } else {
-               // console.log("No se encontraron resultados.");
             }
-    
-           // res.json(cita);
         } catch (error) {
-            //console.error('Error al obtener la cita:', error);
             res.status(500).json({ error: 'Error al obtener la cita' });
         }
     }
